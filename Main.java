a823fbbf72a5fe099dcfd5321aae91e1cf502164
Onga22 <107556703+Onga22@users.noreply.github.com>
2022-06-15 09:50:59 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        suma(10, 50, 48);
+        Coche miCoche = new Coche();
+        miCoche.agregarPuertas();
+        System.out.println(miCoche.puertas);
+
+    }
+
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+
+    }
+}
+
+class Coche {
+
+    int puertas = 4;
+
+    public void agregarPuertas() {
+        this.puertas++;
+    }
+
+}
